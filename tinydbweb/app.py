1d23607b2f214aed463f6058e94210acc395549d
Zeb <zceboys@gmail.com>
2021-02-19 19:03:26 +0000

    Asyncio, many improvements

--- tinydbweb/app.py
+++ tinydbweb/app.py
@@ -1,19 +1,23 @@
 from tinydb import TinyDB, Query
-from flask import Flask, request
+from sanic import Sanic, request, response
 import os
 import hashlib
 from cryptography.fernet import Fernet
 import base64
+import atexit
 
-app = Flask(__name__)
+app = Sanic(__name__)
+
 db = TinyDB("db.json")
+atexit.register(db.close)
+
 password = hashlib.sha1(os.getenv("TDBW_PASSWORD").encode("utf-8")).hexdigest()
-bpassword = base64.urlsafe_b64encode((password[:31]+"=").encode("utf-8"))
+bpassword = base64.urlsafe_b64encode(os.getenv("TDBW_PASSWORD")[:32].encode("utf-8"))
 enc = Fernet(bpassword)
 
 
-def handle_req():
-    json = request.get_json()
+def handle_req(request: request):
+    json = request.json
     if json is None:
         return {}
     if json.get("password") is not None:
@@ -27,6 +31,47 @@ def handle_req():
 
     if json["method"] == "search":
         return {"result": db.search(Query().fragment(json["params"])), "success": True}
+
+    elif json["method"] == "op-search":
+        def opsearch(q):
+            res = True
+            for item, op, const in json["params"]:
+                if not eval(f"q[{repr(item)}] {op} {repr(const)}"):
+                    return False
+            return res
+        return {"success": True, "result": list(filter(opsearch, db.all()))}
+
+    elif json["method"] == "find-search":
+        def find(val):
+            return str(val).lower().find(json["params"]["find"].lower()) >= 0
+        return {
+            "success": True,
+            "result": [x for x in filter(find, db.search(Query().fragment(json["params"]["frag"])))]
+        }
+
+    elif json["method"] == "top-search":
+        greatest = []
+        valids = db.search(Query().fragment(json["params"]["frag"]))
+        measurement = json["params"]["greatest"]
+        max = json["params"]["max"]
+
+        def min():
+            minimum = None
+            for item in greatest:
+                if minimum == None:
+                    minimum = item
+                elif item[measurement] < minimum:
+                    minimum = item
+            return minimum
+
+        for item in valids:
+            minimum = min()
+            if (item[measurement] > minimum) if minimum is not None else True:
+                if len(greatest) >= max:
+                    greatest.remove(minimum)
+                greatest.append(item)
+        return {"success": True, "result": greatest}
+
 
     elif json["method"] == "insert":
         db.insert(json["params"])
@@ -47,12 +92,16 @@ def handle_req():
 
 
 @app.route("/")
-def main():
-    res = handle_req()
+async def main(request: request):
+    res = handle_req(request)
     if res.get("success"):
-        return {"result": enc.encrypt(str(res["result"]).encode("utf-8")), "success": True}
+        return response.json({
+            "result": enc.encrypt(str(res["result"]).encode("utf-8")).decode(),
+            "success": True
+        })
     else:
-        return res
+        return response.json(res)
+
 
 
 if __name__ == "__main__":
